36350e2278d68f1e8e7ca46260bfcd40374efe16
Juan Patiño <juandavid54300@gmail.com>
2026-03-02 10:12:38 -0500

    Fix Header

--- src/features/layout/components/Header.jsx
+++ src/features/layout/components/Header.jsx
@@ -1,41 +1,108 @@
 import * as React from "react";
 import { NavLink } from "react-router-dom";
 
-//MUI ICONS
-import HomeIcon from '@mui/icons-material/Home';
-import ArticleIcon from '@mui/icons-material/Article';
-import LocalOfferIcon from '@mui/icons-material/LocalOffer';
-import PersonIcon from '@mui/icons-material/Person';
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import SearchIcon from '@mui/icons-material/Search';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+// MUI ICONS
+import HomeIcon from "@mui/icons-material/Home";
+import ArticleIcon from "@mui/icons-material/Article";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import PersonIcon from "@mui/icons-material/Person";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import SearchIcon from "@mui/icons-material/Search";
 
-//Mui Componentes
-import {AppBar, Toolbar, Container, Button, Stack, Typography } from  '@mui/material';
-
-
-
+// MUI COMPONENTS
+import {AppBar, Toolbar, Button, Stack, Typography, Box, TextField,} from "@mui/material";
 
 export const Header = () => {
-    return (
-        <AppBar>
-            <Typography variant="h6">Carrito de Compras</Typography>
-            <Typography variant="body1">
-                Bienvenido a nuestro sistema de carrito de compras. Aquí podrás explorar una amplia variedad de productos, agregar tus favoritos al carrito y realizar tus compras de manera fácil y segura. ¡Disfruta de tu experiencia de compra con nosotros!
-            </Typography>
-            <Stack direction="row">
-                <Button variant="contained" component={NavLink} to="/" startIcon={<HomeIcon />}>Inicio</Button>
-                <Button component={NavLink} to="/article" startIcon={<ArticleIcon />}>Artículos</Button>
-                <Button component={NavLink} to="/offers" startIcon={<LocalOfferIcon />}>Ofertas</Button>
-                <Button component={NavLink} to="/account" startIcon={<PersonIcon />}>Mi Cuenta</Button>
-                <Button component={NavLink} to="/purchases" startIcon={<ShoppingBagIcon />}>Mis Compras</Button>
-                <Button component={NavLink} to="/favorites" startIcon={<FavoriteIcon />}>Mis Favoritos</Button>
-            </Stack>
-            <form role="search">
-                <input type="search" placeholder="Buscar Productos..." />
-                <Button variant="contained" color="primary" endIcon={<SearchIcon />}>Buscar</Button>
-            </form>
-        </AppBar>
-    );
+  return (
+    <>
+      <AppBar position="fixed" color="primary">
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          
+          {/* LEFT SIDE */}
+          <Stack direction="row" spacing={1}>
+            <Button
+              color="inherit"
+              component={NavLink}
+              to="/"
+              startIcon={<HomeIcon />}
+            >
+              Inicio
+            </Button>
+
+            <Button
+              color="inherit"
+              component={NavLink}
+              to="/article"
+              startIcon={<ArticleIcon />}
+            >
+              Artículos
+            </Button>
+
+            <Button
+              color="inherit"
+              component={NavLink}
+              to="/offers"
+              startIcon={<LocalOfferIcon />}
+            >
+              Ofertas
+            </Button>
+
+            <Button
+              color="inherit"
+              component={NavLink}
+              to="/account"
+              startIcon={<PersonIcon />}
+            >
+              Mi Cuenta
+            </Button>
+
+            <Button
+              color="inherit"
+              component={NavLink}
+              to="/purchases"
+              startIcon={<ShoppingBagIcon />}
+            >
+              Mis Compras
+            </Button>
+
+            <Button
+              color="inherit"
+              component={NavLink}
+              to="/favorites"
+              startIcon={<FavoriteIcon />}
+            >
+              Favoritos
+            </Button>
+          </Stack>
+
+          {/* RIGHT SIDE - SEARCH */}
+          <Box
+            component="form"
+            sx={{ display: "flex", alignItems: "center", gap: 1 }}
+          >
+            <TextField
+              size="small"
+              variant="outlined"
+              placeholder="Buscar productos..."
+              sx={{
+                bgcolor: "white",
+                borderRadius: 1,
+              }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              endIcon={<SearchIcon />}
+            >
+              Buscar
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      {/* Esto evita que el contenido quede debajo del AppBar */}
+      <Toolbar />
+    </>
+  );
 };
