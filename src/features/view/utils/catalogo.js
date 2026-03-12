fe72379268ca2517e9072d43fccd098108f7b454
JuanUrrego02 <juandavid54300@gmail.com>
2026-03-11 20:59:51 -0500

    Final Version3

--- src/features/view/utils/catalogo.js
+++ src/features/view/utils/catalogo.js
@@ -1,46 +1,45 @@
 export const products = [
-    {
-      title: "Camiseta Clemont - Negra",
-      description: "Precio: $150.000",
-      image: "../public/img/1.jpg",
-    },
-    {
-      title: "Pantalón Purple - Claro",
-      description: "Precio: $200.000",
-      image: "../public/img/2.jpg",
-    },
-    {
-      title: "Buzo UnderGold - Negro",
-      description: "Precio: $180.000",
-      image: "../public/img/3.jpg",
-    },
-    {
-      title: "Camiseta UnderGold - Blanca",
-      description: "Precio: $120.000",
-      image: "../public/img/4.jpg",
-    },
-    {
-      title: "Buzo Y/Out - Negro",
-      description: "Precio: $180.000",
-      image: "../public/img/5.jpg",
-    },
-    {
-      title: "Mocho Amiri - Claro",
-      description: "Precio: $120.000",
-      image: "../public/img/6.jpg"
-    },
-    {
-      title: "Conjunto Y/Out",
-      description: "Precio: $180.000",
-      image: "../public/img/7.jpg"
-
-    },
-    {
-      title: "Buzo Coach - Negro",
-      description: "Precio: $120.000",
-      image: "../public/img/9.jpg"
-    },
-  ];
+  {
+    title: "Camiseta Clemont - Negra",
+    description: "Precio: $150.000",
+    image: "/img/1.jpg",
+  },
+  {
+    title: "Pantalón Purple - Claro",
+    description: "Precio: $200.000",
+    image: "/img/2.jpg",
+  },
+  {
+    title: "Buzo UnderGold - Negro",
+    description: "Precio: $180.000",
+    image: "/img/3.jpg",
+  },
+  {
+    title: "Camiseta UnderGold - Blanca",
+    description: "Precio: $120.000",
+    image: "/img/4.jpg",
+  },
+  {
+    title: "Buzo Y/Out - Negro",
+    description: "Precio: $180.000",
+    image: "/img/5.jpg",
+  },
+  {
+    title: "Mocho Amiri - Claro",
+    description: "Precio: $120.000",
+    image: "/img/6.jpg",
+  },
+  {
+    title: "Conjunto Y/Out",
+    description: "Precio: $180.000",
+    image: "/img/7.jpg",
+  },
+  {
+    title: "Buzo Coach - Negro",
+    description: "Precio: $120.000",
+    image: "/img/8.jpg",
+  },
+];
 
 export const formatPrice = (value) =>
   new Intl.NumberFormat("es-CO", {
